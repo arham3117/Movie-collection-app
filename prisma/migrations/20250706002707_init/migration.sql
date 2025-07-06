0b2a13a873df70a6163b8405046a9c1fe49a24f2
Muhammad Arham <arhamctc3117@gmail.com>
2025-07-06 17:34:19 -0500

    Migrate to MySQL database for two-tier architecture

--- prisma/migrations/20250706002707_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Movie" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "producer" TEXT NOT NULL,
-    "releaseDate" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
